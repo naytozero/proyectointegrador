018fb14ff5ec6213b88c2cc5bfaa44db58a0d9e6
naytozero <zusumillaa@gmail.com>
2020-12-22 03:17:30 +0000

    proyecto

--- Proyecto_integrador/src/main/java/App.java
+++ Proyecto_integrador/src/main/java/App.java
@@ -2,10 +2,7 @@
 
    public class Main {
 
- /** * @CodigosJava.BlogSpot.com
- * JFrame con Usuario y Contraseña para acceder al sistema
- *Codigo de fuente Java
- */
+ 
  public static void main(String[] args) {
  Login EjecuteLogin = new Login();
  EjecuteLogin.setVisible(true);
